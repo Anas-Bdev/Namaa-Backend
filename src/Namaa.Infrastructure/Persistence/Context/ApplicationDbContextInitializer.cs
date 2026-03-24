e02ccb6df15e5c338b0b2c037f457206db10a550
Anas <anashaj04@gmail.com>
2026-03-25 00:59:12 +0200

    feat: complete LandsController API and implement role database seeder

--- src/Namaa.Infrastructure/Persistence/Context/ApplicationDbContextInitializer.cs
+++ src/Namaa.Infrastructure/Persistence/Context/ApplicationDbContextInitializer.cs
@@ -1,10 +1,23 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using Namaa.Domain.Common.Constants;
+using Namaa.Infrastructure.Identity;
 
 namespace Namaa.Infrastructure.Persistence.Context;
-public class ApplicationDbContextInitializer
+public class ApplicationDbContextInitializer(RoleManager<AppRole> roleManager)
 {
-    // public async Task TrySeedAsync
+    public async Task TrySeedAsync()
+    {
+        var roles=new[] {AppRoles.Admin,AppRoles.Expert,AppRoles.Farmer,AppRoles.Trader,AppRoles.Investor};
+        foreach(var role in roles)
+        {
+            if (! await roleManager.RoleExistsAsync(role))
+            {
+                await roleManager.CreateAsync(new AppRole {Name=role});
+            }
+        }
+    }
 }
 public static class InitializerExtensions
 {
@@ -12,6 +25,7 @@ public static class InitializerExtensions
     {
         using var scope=app.Services.CreateScope();
         var initializer=scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitializer>();
+        await initializer.TrySeedAsync();
 
     }
 }
