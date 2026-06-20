1440cd6cd96fcdf0af073f4f089d11912904df10
Anas <anashaj04@gmail.com>
2026-06-20 22:49:12 +0300

    AddPhoneNumberForInvestor

--- src/Namaa.Application/Features/Investors/Dtos/InvestorListItemDto.cs
+++ src/Namaa.Application/Features/Investors/Dtos/InvestorListItemDto.cs
@@ -4,6 +4,7 @@ public class InvestorListItemDto
     public Guid Id {get;set;}
     public string FullName {get;set;}=string.Empty;
     public string? OrganizationName {get;set;}
+    public string? PhoneNumber {get;set;}
     public string InvestorType {get;set;}=string.Empty;
     public string Governorate {get;set;}=string.Empty;
     public string?  ProfileImageUrl {get;set;}
